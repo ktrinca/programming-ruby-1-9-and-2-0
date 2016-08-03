b54f1d009c1b587a07b585795616b72dbc3454cc
ktrinca <yraspo@gmail.com>
2016-08-03 10:25:34 -0300

    Add attributes Add price_in_cents method

--- chapter-3/tut_clases/book_in_stock.rb
+++ chapter-3/tut_clases/book_in_stock.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/ruby
 class BookInStock
+  attr_reader :isbn
+  attr_accessor :price
+  
   def initialize(isbn, price)
     @isbn = isbn
     @price = Float(price)
+  end
+  def price_in_cents
+    Integer(price*100 + 0.5)
+  end
+  def price_in_cents=(cents)
+    @price = cents / 100.0
   end
 end  
